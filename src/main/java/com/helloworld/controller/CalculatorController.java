3ee38eac0bd49018f3b0877f9012a1e315cfb069
Bruno Henrique <brunohsfrancisco@gmail.com>
2022-08-16 22:12:59 -0300

    implementa operacoes calculator

--- src/main/java/com/helloworld/controller/CalculatorController.java
+++ src/main/java/com/helloworld/controller/CalculatorController.java
@@ -15,4 +15,28 @@ public class CalculatorController {
         return CalculatorModel.sum(Float.parseFloat(n1), Float.parseFloat(n2));
     }
 
+    @GetMapping("/sub/{n1}/{n2}")
+    public float sub(
+            @PathVariable("n1") String n1,
+            @PathVariable("n2") String n2
+    ){
+        return CalculatorModel.sub(Float.parseFloat(n1), Float.parseFloat(n2));
+    }
+
+    @GetMapping("/mult/{n1}/{n2}")
+    public float mult(
+            @PathVariable("n1") String n1,
+            @PathVariable("n2") String n2
+    ){
+        return CalculatorModel.mult(Float.parseFloat(n1), Float.parseFloat(n2));
+    }
+
+    @GetMapping("/div/{n1}/{n2}")
+    public float div(
+            @PathVariable("n1") String n1,
+            @PathVariable("n2") String n2
+    ){
+        return CalculatorModel.div(Float.parseFloat(n1), Float.parseFloat(n2));
+    }
+
 }
--- src/main/java/com/helloworld/model/CalculatorModel.java
+++ src/main/java/com/helloworld/model/CalculatorModel.java
@@ -8,4 +8,21 @@ public class CalculatorModel {
 
     }
 
+    public static float sub (float n1, float n2) {
+
+         return n1-n2;
+
+    }
+
+    public static float mult (float n1, float n2) {
+
+        return n1*n2;
+
+    }
+
+    public static  float div (float n1, float n2) {
+
+        return n1/n2;
+    }
+
 }
